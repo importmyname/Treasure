97083df0f010a08f295264894741ac828bd2bacd
importmyname <copsmach@gmail.com>
2022-04-13 01:11:10 +0700

    Add Image slider

--- Project/Image-slider/Image-slider.js
+++ Project/Image-slider/Image-slider.js
@@ -1,1 +1,35 @@
+ const item = document.querySelectorAll('img') ;
+ const itemCount = item.length;
+ const nextItem = document.querySelector('.next') ;
+ const previousItem = document.querySelector('.previous')
+ let count = 0 ;
  
+ function showNextItem() {
+     item[count].classList.remove('active') ;
+     
+     if (count < itemCount - 1) {
+         count++ ;
+     } else {
+         count = 0 ;
+     }
+
+     item[count].classList.add('active') ;
+
+ }
+ function showPreviousItem() {
+     item[count].classList.remove('active') ;
+
+     if (count <  0 ) {
+         count-- ;
+     } else {
+         count = itemCount - 1 ;
+     }
+
+     item[count].classList.add('active') ;
+
+ }
+
+ nextItem.addEventListener('click', showNextItem) ;
+ nextItem.addEventListener('click', showpreviousItem ) ;
+
+
